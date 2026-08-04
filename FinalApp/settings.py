f57fffc80d2282aedb6744870e51e7d1bf677b4f
Dkomeyan92 <davidetta.komeyan01@utrgv.edu>
2026-08-04 15:13:54 -0400

    Complete Sprint 2 user accounts and MCT branding

--- FinalApp/settings.py
+++ FinalApp/settings.py
@@ -115,9 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-LSTATIC_URL = 'static/'
 
 # User authentication redirects
 LOGIN_REDIRECT_URL = 'profile'
